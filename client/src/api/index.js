897e37a97463b1f781066350c644e9ec7b95b93f
5top1t <jarryngandjui@gmail.com>
2020-06-27 18:16:40 -0400

    fixes .com to .io

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 const url = process.env.NODE_ENV === 'development' 
     ? 'localhost:3000'
-    : 'beantowncaffe.com'
+    : 'beantowncaffe.io'
 
 const api = axios.create({
   baseURL: `http://${url}/api`
